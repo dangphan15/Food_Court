000610f873f34567a50de8f495fc8cd4a9182377
dangphan15 <minhdang12345c@gmail.com>
2023-06-24 15:46:58 +0700

    add something

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route, Link } from "react-router-dom";
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.css';
+import './css/main.css';
+import './css/normalize.css';
+import './css/sb-admin-2.css';
+import './css/sb-admin-2.min.css';
+import UserList from "./pages/User/user-list";
+import ProductList from "./pages/Product/product-list";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Login/>
+      // <div id="wrapper">
+      //     <Navbar />
+      //     <div id="content-wrapper" className="d-flex flex-column">
+      //         <div id="content">
+      //             <Header />
+      //             <Routes>
+      //                 <Route path="/" element={<UserList/>} />
+      //                 <Route path="/users" element={<UserList/>} />
+      //                 <Route path="/products" element={<ProductList/>} />
+      //             </Routes>
+      //         </div>
+      //     </div>
+      // </div>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+            <Link to={"/"} className="sidebar-brand d-flex align-items-center justify-content-center">
+                <div className="sidebar-brand-text mx-3">Dashboard</div>
+            </Link>
+            <hr className="sidebar-divider my-0"/>
+
+                <li className="nav-item active">
+                    <Link to={"/"} className="nav-link">
+                        <i className="fas fa-fw fa-tachometer-alt"></i>
+                        <span>Dashboard</span>
+                    </Link>
+                </li>
+
+                <hr className="sidebar-divider"/>
+
+                    <li className="nav-item">
+                        <Link to={"/users"} className="nav-link">
+                            <i className="fas fa-fw fa-table"></i>
+                            <span>User</span>
+                        </Link>
+                    </li>
+
+                    <li className="nav-item">
+                        <Link to={"/products"} className="nav-link">
+                            <i className="fas fa-fw fa-table"></i>
+                            <span>Product</span>
+                        </Link>
+                    </li>
+
+                    <hr className="sidebar-divider d-none d-md-block"/>
+
+                        <div className="text-center d-none d-md-inline">
+                            <button className="rounded-circle border-0" id="sidebarToggle"></button>
+                        </div>
+        </ul>
+    );
+};
+
+export default Navbar;
